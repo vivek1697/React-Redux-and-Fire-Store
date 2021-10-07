7a03ed31c7466403a6b8746d5924d4a6e7ff5eb3
Vivek Patel <vivek.d.ptl@gmail.com>
2021-10-06 20:07:28 -0400

    Intiated routing functionality for the app

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -3,6 +3,10 @@ import React, {useState} from 'react';
 import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar';
 import { Container } from 'semantic-ui-react';
+import { Route } from 'react-router-dom';
+import Homepage from '../../features/home/HomePage';
+import EventDetailPage from '../../features/events/eventDetailed/EventDetailedPage';
+import EventForm from '../../features/events/eventForm/EventForm';
 
 function App() {
   const [formOpen, setFormOpen] = useState(false);
@@ -25,12 +29,10 @@ function App() {
     <div>
       <NavBar setFormOpen={handleCreateFormOpen} />
       <Container className="main">
-        <EventDashboard
-          formOpen={formOpen}
-          setFormOpen={setFormOpen}
-          selectEvent={handleSelectEvent}
-          selectedEvent={selectedEvent}
-        />
+        <Route exact path='/' component={Homepage}/>
+        <Route exact path='/events' component={EventDashboard}/>
+        <Route path='/events/:id' component={EventDetailPage}/>
+        <Route path='/createEvent' component={EventForm}/>
       </Container>
     </div>
   );
