86e8f5e608ecc57d3c78dc0bff14e5800283de6e
Vivek Patel <vivek.d.ptl@gmail.com>
2021-10-10 14:58:06 -0400

    Fixed manage button action

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -1,35 +1,34 @@
 import React from 'react';
-
 import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar';
 import { Container } from 'semantic-ui-react';
 import { Route, useLocation } from 'react-router-dom';
-import Homepage from '../../features/home/HomePage';
-import EventDetailPage from '../../features/events/eventDetailed/EventDetailedPage';
+import HomePage from '../../features/home/HomePage';
+import EventDetailedPage from '../../features/events/eventDetailed/EventDetailedPage';
 import EventForm from '../../features/events/eventForm/EventForm';
 import Sandbox from '../../features/sandbox/Sandbox';
 
 export default function App() {
   const {key} = useLocation();
-  
+
   return (
-    <div>
-      <Route exact path="/" component={Homepage} />
+    <>
+      <Route exact path='/' component={HomePage} />
       <Route
-        path={"/(.+)"}
+        path={'/(.+)'}
         render={() => (
-          <div>
-            <NavBar  />
-            <Container className="main">
-              <Route exact path="/events" component={EventDashboard} />
-              <Route exact path="/sandbox" component={Sandbox} />
-              <Route path="/events/:id" component={EventDetailPage} />
-              <Route path={["/createEvent", "manage/:id"]} component={EventForm} />
+          <>
+            <NavBar />
+            <Container className='main'>
+              <Route exact path='/events' component={EventDashboard} />
+              <Route exact path='/sandbox' component={Sandbox} />
+              <Route path='/events/:id' component={EventDetailedPage} />
+              <Route path={['/createEvent', '/manage/:id']} component={EventForm} key={key} />
             </Container>
-          </div>
+          </>
         )}
       />
-    </div>
+    </>
   );
 }
 
